9bb13b8fd82fded952598fa84dbe06ab433657c0
Lasya <lsypry@gmail.com>
2016-05-16 22:41:37 -0700

    added node packages and removed unnecessary sample code

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 #!/bin/env node
-//  OpenShift sample Node application
+
 var express = require('express');
-var fs      = require('fs');
+var logger = require('morgan');
+var template = require('pug');
 
-
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
-
-    //  Scope.
-    var self = this;
-
+var MainApp = function() {
+	
+	var self = this;
 
     /*  ================================================================  */
     /*  Helper functions.                                                 */
@@ -33,26 +28,20 @@ var SampleApp = function() {
         };
     };
 
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    self.compileTemplates = function() {
+    	self.templates = {};
+    	var templateDir = __dirname + '/source/templates/'
+    	self.templates['login'] = template.compileFile(templateDir + 'login.pug');
     };
 
-
     /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
+     *  Initializes the sample application.
      */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
+    self.initialize = function() {
+        self.setupVariables();
+        self.setupTerminationHandlers();
+        self.compileTemplates();
+    };
 
     /**
      *  terminator === the termination handler
@@ -84,56 +73,18 @@ var SampleApp = function() {
         });
     };
 
+    self.app = express();
+    self.app.use(logger('dev'))
+    self.app.use(express.static(__dirname + '/static'));
 
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
+    self.app.get('/', function(req, res, next) {
+    	try {
+    		var html = self.templates['login']();
+    		res.send(html);
+    	} catch (e) {
+    		next(e);
+    	}
+    });
 
     /**
      *  Start the server (starts up the sample application).
@@ -146,14 +97,10 @@ var SampleApp = function() {
         });
     };
 
-};   /*  Sample Application.  */
+};
 
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
+var zapp = new MainApp();
 zapp.initialize();
 zapp.start();
+
 
